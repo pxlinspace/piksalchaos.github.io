01f562c79d83eb15d5bfc51d690eb57152738f55
piksalchaos <108901892+piksalchaos@users.noreply.github.com>
2026-05-28 22:02:13 -0700

    can now drag idiots around canvas

--- js/idiot-canvas.js
+++ js/idiot-canvas.js
@@ -29,31 +29,64 @@ let idiots = [];
 
 class Idiot {
   static SPEED = 75;
+  static GRAVITY = 500;
   static RECT_SIZE = 16;
   static SPRITE_SCALE = 3;
-  static FRAME_COUNT = 4;
-  static ANIMATION_TIME = 125;
+  static FRAME_COUNTS = [4, 2];
+  static ANIMATION_TIMES = [125, 75];
+  static getSize() {
+    return Idiot.RECT_SIZE * Idiot.SPRITE_SCALE;
+  }
 
   constructor(x, y) {
     this.x = x;
     this.y = y;
+    this.yVelocity = 0;
+    this.animationRow = 0;
     this.frame = 0;
     this.animationIntervalId = null;
+    this._isGrabbed = false;
     this.isMoving = false;
+    this.movementTimeoutId = null;
     this.xDirection = 1;
     this.continueMovementProcess();
   }
 
   update(dt) {
+    if (!this.isGrabbed) {
+      if (this.y < canvas.height - Idiot.getSize() * 0.5) {
+        this.yVelocity += Idiot.GRAVITY * dt;
+        this.y += this.yVelocity * dt;
+      } else {
+        this.y = canvas.height - Idiot.getSize() * 0.5;
+        this.yVelocity = 0;
+
+        if (this.animationRow == 1) {
+          this.animationRow = 0;
+          this.frame = 0;
+          clearInterval(this.animationIntervalId);
+          this.animationIntervalId = null;
+        }
+
+        if (!this.movementTimeoutId) {
+          this.animationIntervalId = null;
+          this.continueMovementProcess();
+        }
+      }
+    }
+
     if (this.isMoving) {
       this.x += Idiot.SPEED * dt * this.xDirection;
       if (this.animationIntervalId === null) {
         this.animationIntervalId = setInterval(
           this.gotoNextFrame.bind(this),
-          Idiot.ANIMATION_TIME,
+          Idiot.ANIMATION_TIMES[this.animationRow],
         );
       }
-    } else {
+    } else if (
+      !this.isGrabbed &&
+      this.y === canvas.height - Idiot.getSize() * 0.5
+    ) {
       if (this.animationIntervalId !== null) {
         clearInterval(this.animationIntervalId);
         this.animationIntervalId = null;
@@ -64,13 +97,13 @@ class Idiot {
 
   gotoNextFrame() {
     this.frame += 1;
-    if (this.frame >= Idiot.FRAME_COUNT) {
+    if (this.frame >= Idiot.FRAME_COUNTS[this.animationRow]) {
       this.frame = 0;
     }
   }
 
   continueMovementProcess() {
-    setTimeout(
+    this.movementTimeoutId = setTimeout(
       () => {
         this.continueMovementProcess();
         this.isMoving = !this.isMoving;
@@ -93,22 +126,44 @@ class Idiot {
     context.save();
     context.translate(this.x, this.y);
     context.scale(this.xDirection, 1);
+
+    const size = Idiot.getSize();
     context.drawImage(
       image,
       this.frame * Idiot.RECT_SIZE,
-      0,
+      this.animationRow * Idiot.RECT_SIZE,
       Idiot.RECT_SIZE,
       Idiot.RECT_SIZE,
-      -Idiot.RECT_SIZE * Idiot.SPRITE_SCALE * 0.5,
-      0,
-      Idiot.RECT_SIZE * Idiot.SPRITE_SCALE,
-      Idiot.RECT_SIZE * Idiot.SPRITE_SCALE,
+      -size * 0.5,
+      -size * 0.5,
+      size,
+      size,
     );
     context.restore();
   }
 
-  getAlpha() {
-    return (Math.sin(this.alphaRadians) + 1) / 2;
+  get isGrabbed() {
+    return this._isGrabbed;
+  }
+
+  set isGrabbed(value) {
+    this._isGrabbed = value;
+    if (value) {
+      this.isMoving = false;
+      this.frame = 0;
+      if (this.movementTimeoutId) {
+        clearTimeout(this.movementTimeoutId);
+        this.movementTimeoutId = null;
+      }
+      this.animationRow = 1;
+      clearInterval(this.animationIntervalId);
+      this.animationIntervalId = setInterval(
+        this.gotoNextFrame.bind(this),
+        Idiot.ANIMATION_TIMES[this.animationRow],
+      );
+    } else {
+      this.yVelocity = 0;
+    }
   }
 }
 
@@ -136,8 +191,62 @@ function draw() {
 
 function initialize() {
   for (let i = 0; i < 3; i++) {
-    idiots.push(new Idiot(Math.random() * canvas.width, 2));
+    idiots.push(
+      new Idiot(
+        Math.random() * canvas.width,
+        canvas.height - Idiot.getSize() * 0.5,
+      ),
+    );
   }
 }
 
 requestAnimationFrame(update);
+
+const MAX_MOUSE_DISTANCE = 50;
+addEventListener("mousedown", (event) => {
+  if (event.button !== 0) return;
+
+  const canvasRect = canvas.getBoundingClientRect();
+
+  let mouseX = event.clientX - canvasRect.left;
+  let mouseY = event.clientY - canvasRect.top;
+
+  let closestIdiotIndex = null;
+  let closestDistance = Infinity;
+  for (let i = 0; i < idiots.length; i++) {
+    const distance = Math.hypot(mouseX - idiots[i].x, mouseY - idiots[i].y);
+    if (distance < closestDistance && distance < MAX_MOUSE_DISTANCE) {
+      closestIdiotIndex = i;
+      closestDistance = distance;
+    }
+  }
+  if (closestIdiotIndex !== null) {
+    idiots[closestIdiotIndex].isGrabbed = true;
+    setIdiotToMouse(idiots[closestIdiotIndex], mouseX, mouseY);
+  }
+});
+addEventListener("mouseup", (event) => {
+  if (event.button !== 0) return;
+
+  for (let i = 0; i < idiots.length; i++) {
+    idiots[i].isGrabbed = false;
+  }
+});
+
+addEventListener("mousemove", (event) => {
+  const canvasRect = canvas.getBoundingClientRect();
+  let mouseX = event.clientX - canvasRect.left;
+  let mouseY = event.clientY - canvasRect.top;
+  for (let i = 0; i < idiots.length; i++) {
+    if (idiots[i].isGrabbed) {
+      setIdiotToMouse(idiots[i], mouseX, mouseY);
+    }
+  }
+  draw();
+});
+
+function setIdiotToMouse(idiot, mouseX, mouseY) {
+  const distance = Idiot.getSize() * 0.5;
+  idiot.x = Math.max(distance, Math.min(canvas.width - distance, mouseX));
+  idiot.y = Math.max(distance, Math.min(canvas.height - distance, mouseY));
+}
